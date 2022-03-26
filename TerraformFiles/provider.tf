d9337193b889230b44058b49b4b4dec28a4dab8e
Stanley Godspower <godspowerstanley@gmail.com>
2022-03-26 18:49:11 +0100

    update-actions

--- TerraformFiles/provider.tf
+++ TerraformFiles/provider.tf
@@ -17,7 +17,7 @@ data "azurerm_resource_group" "rg" {
   name = var.rg_name
 }
 
-data "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = data.azurerm_resource_group.rg.name
-}
+# data "azurerm_container_registry" "acr" {
+#   name                = var.acr_name
+#   resource_group_name = data.azurerm_resource_group.rg.name
+# }
